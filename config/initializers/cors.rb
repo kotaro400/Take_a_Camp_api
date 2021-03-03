658c2cc80f64a20c7bd9f09acef091ac0cc595a2
kotaro <ktrst0622@gmail.com>
2021-03-03 20:34:03 +0900

    cors修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '192.168.11.3'
+    origins 'http://192.168.11.3:3000'
 
     resource '*',
       headers: :any,
