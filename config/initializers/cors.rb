2b73ce9a104bb6ed69fa5808a754080d6cad3f83
kotaro <ktrst0622@gmail.com>
2021-03-04 19:17:28 +0900

    cors修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://take-a-camp.herokuapp.com'
+    origins 'https://take-a-camp.herokuapp.com', 'https://d3hlpxqlclicxz.cloudfront.net'
 
     resource '*',
       headers: :any,
