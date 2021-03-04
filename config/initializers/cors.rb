13a1d5baf4952690e484e3d11d7ee8b9e1e8a5ac
kotaro <ktrst0622@gmail.com>
2021-03-04 18:34:03 +0900

    cors originの変更

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://192.168.11.3:3000'
+    origins 'https://d3hlpxqlclicxz.cloudfront.net'
 
     resource '*',
       headers: :any,
