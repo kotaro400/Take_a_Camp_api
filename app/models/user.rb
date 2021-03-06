9e7a41f74ab2121d761313e8c04af85c3306e737
kotaro <ktrst0622@gmail.com>
2021-03-06 23:57:47 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
 
   validates :password, presence: true, length: { in: 6..20 }
   validates :name, presence: true, uniqueness: { case_sensitive: true }
-  validates_format_of [:name, :password], with: /\A[a-zA-Z0-9]+\z/
+  validates_format_of :password, with: /\A[a-zA-Z0-9]+\z/
 end
